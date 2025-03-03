36fdc0f5c9df0d078a814d95b4fc9448b319c14a
delvinkhairan <delvinkhairan9558@gmail.com>
2025-03-03 21:54:02 +0700

    tambahan fitur GUI

--- Program.cs
+++ Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-namespace TPmod03_KPL
+//nim: 103022300030
+namespace mod3_KPL
 {
-    static class Program
+    public class MainForm : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private TextBox inputTextBox;
+        private Button submitButton;
+        private Label outputLabel;
+
+        public MainForm()
+        {
+            this.Text = "Simple GUI";
+            this.Size = new System.Drawing.Size(400, 200);
+
+            inputTextBox = new TextBox();
+            inputTextBox.Location = new System.Drawing.Point(20, 20);
+            inputTextBox.Width = 250;
+
+            submitButton = new Button();
+            submitButton.Text = "Submit/Kirim";
+            submitButton.Location = new System.Drawing.Point(280, 18);
+            submitButton.Click += new EventHandler(SubmitButton_Click);
+
+            outputLabel = new Label();
+            outputLabel.Location = new System.Drawing.Point(20, 60);
+            outputLabel.AutoSize = true;
+
+            this.Controls.Add(inputTextBox);
+            this.Controls.Add(submitButton);
+            this.Controls.Add(outputLabel);
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string inputText = inputTextBox.Text;
+            outputLabel.Text = $"Halo {inputText}";
+        }
+
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
